e3ac0f9fb487cf83df331d86829f00124ec3d368
Jhonas Alves <jhonas.alvesm@gmail.com>
2021-10-26 09:27:17 -0300

    Add project files.

---
+++ IImpostoTerritorial.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace STUR
+{
+    [ServiceContract]
+    public interface IImpostoTerritorial
+    {
+
+        [OperationContract]
+        ImpostoDTO ConsultarImpostoTerritorial(int identificacaoImovel, string documento);
+    }
+
+    [DataContract]
+    public class ImpostoDTO
+    {
+        /// <summary>
+        /// Identificação do Imóvel
+        /// </summary>
+        [DataMember]
+        public int IdentificacaoImovel { get; set; }
+        /// <summary>
+        /// CPF/CNPJ
+        /// </summary>
+        [DataMember]
+        public string Documento { get; set; }
+        /// <summary>
+        /// Ano de Exercicio
+        /// </summary>
+        [DataMember]
+        public int AnoExercicio { get; set; }
+        /// <summary>
+        /// Valor
+        /// </summary>
+        [DataMember]
+        public decimal Valor { get; set; }
+        /// <summary>
+        /// Endereço
+        /// </summary>
+        [DataMember]
+        public EnderecoDTO Endereco { get; set; }
+    }
+
+    public class EnderecoDTO
+    {
+        /// <summary>
+        /// CEP
+        /// </summary>
+        public string Cep { get; set; }
+        /// <summary>
+        /// Endereço
+        /// </summary>
+        public string Endereco { get; set; }
+        /// <summary>
+        /// Bairro
+        /// </summary>
+        public string Bairro { get; set; }
+        /// <summary>
+        /// Número
+        /// </summary>
+        public string Numero { get; set; }
+        /// <summary>
+        /// Complemento
+        /// </summary>
+        public string Complemento { get; set; }
+    }
+}
---
+++ ImpostoTerritorial.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace STUR
+{
+    public class ImpostoTerritorial : IImpostoTerritorial
+    {
+        public ImpostoDTO ConsultarImpostoTerritorial(int identificacaolImovel, string documento)
+        {
+
+            return new ImpostoDTO {
+                Documento = documento,
+                IdentificacaoImovel = identificacaolImovel,
+                AnoExercicio = DateTime.Now.Year,
+                Valor = new Random().Next(100, 1000),
+                Endereco = new EnderecoDTO
+                {
+                    Cep = "12345789",
+                    Endereco = "Av. Brasil",
+                    Bairro = "Centro",
+                    Numero = "123",
+                    Complemento = ""
+                }
+            };
+        }
+
+
+    }
+}
